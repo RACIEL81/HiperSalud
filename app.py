776e47efaa0fd3295a255ed99d0cccf6c08b4bf7
RACIEL81 <98654451+RACIEL81@users.noreply.github.com>
2025-05-04 12:14:58 -0500

    web

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
